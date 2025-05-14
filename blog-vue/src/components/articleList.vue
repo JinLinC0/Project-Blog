902980f75d4de838f855d5b80ff2517e5ac3ce91
jlc <2794810071@qq.com>
2025-05-14 17:29:47 +0800

    update：首页样式的修改

--- blog-vue/src/components/articleList.vue
+++ blog-vue/src/components/articleList.vue
@@ -3,8 +3,8 @@
         <section v-for="(article, index) in pageResult?.data" :key="article.id" :data-index="index">
             <div class="md:h-[150px] h-[200px]">
                 <div class="mt-2 flex justify-between">
-                    <div @click="$router.push(`/article/${article.id}`)" class="cursor-pointer py-2 px-3 mb-2 w-full">
-                        {{ article.title }}
+                    <div @click="$router.push(`/article/${article.id}`)" class="cursor-pointer py-2 px-3 mb-2 w-full hover:text-blue-500">
+                        {{ article.title.length > 10 ? article.title.substring(0, 10) + '...' : article.title }}
                     </div>
                     <el-button class="w-1/10 flex-shrink-0" color="#626aef" plain
                         @click="$router.push(`/category/${article.categoryId}`)">
